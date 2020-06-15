e98c6ee844d86d61b56b8955b5a324ec88124482
Alexander Roe <alexanderroe114@gmail.com>
2020-06-15 17:51:50 -0500

    major visual changes

--- index.js
+++
@@ -1,7 +0,0 @@
-function changeContent(tabName) {
-    var tabs = document.getElementsByClassName("content");
-    for (var i = 0; i < tabs.length; i++) {
-        tabs[i].style.display = "none";
-    }
-    document.getElementById(tabName).style.display = "block";
-}
